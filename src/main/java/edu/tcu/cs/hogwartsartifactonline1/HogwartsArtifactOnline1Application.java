65f5fa1e3b3dc67e0b7f74214343f01b67702c04
Ben Hartje <bhartje@gmail.com>
2025-03-27 12:54:43 -0500

    This is the first commit

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactonline1/HogwartsArtifactOnline1Application.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartifactonline1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtifactOnline1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsArtifactOnline1Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/hogwartsartifactonline1/HogwartsArtifactOnline1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartifactonline1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactOnline1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
